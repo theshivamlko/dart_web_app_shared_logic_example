84fbc9574670df4985b72a342c2334f40d0e5bd7
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2024-12-25 02:27:59 +0530

    dart interop to js working

---
+++ src/upperCaseDartCode.dart
@@ -0,0 +1,19 @@
+
+
+
+import 'dart:js_interop';
+import 'dart:js';
+
+void main() {
+  _toUpperCaseEachWord=toUpperCaseEachWord.toJS;
+}
+
+@JS("toUpperCaseEachWord")
+external set _toUpperCaseEachWord(JSFunction value);
+
+
+String toUpperCaseEachWord(String input) {
+  List<String> words = input.split(" ");
+  List<String> capitalizedWords = words.map((word) => word.isNotEmpty ? word[0].toUpperCase() + word.substring(1) : "").toList();
+  return capitalizedWords.join(" ");
+}
